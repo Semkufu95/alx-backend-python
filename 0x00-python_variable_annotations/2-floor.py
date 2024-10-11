61a28f56384156e3916e45242683da0eaf761547
semkufu95 <semkufu95@gmail.com>
2024-10-11 04:36:00 +0300

    Task 2 - floor of an int

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Type annotated func floor that takes a float n and returns a floor of the float
+Note: All floor are integers
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Takes a float argument and return a floor int
+    """
+    return int(n)
