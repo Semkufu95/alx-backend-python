fa76cb93faad43ba60ae6b14f34979d5fc6bff94
semkufu95 <semkufu95@gmail.com>
2024-10-11 04:20:58 +0300

    Task 1 - string concatenation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-A type annotated function 'add' that takes a float 'a' and 'a' and return a sum as floats
+A type annotated function 'add' that takes a float 'a' and 'b'
+ and return a sum as floats
 """
+
 
 def add(a: float, b: float) -> float:
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+A type annotated function 'concat' that takes a string str1 and str2
+and returns a concatenated argument
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
