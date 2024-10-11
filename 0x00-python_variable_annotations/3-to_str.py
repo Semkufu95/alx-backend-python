689162ad47303513905ec267ffed0100126dbbce
semkufu95 <semkufu95@gmail.com>
2024-10-11 04:40:25 +0300

    Task 3 - float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A type annotated function to_str that takes a float 'n'
+and returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns float to str
+    """
+    return str(n)
